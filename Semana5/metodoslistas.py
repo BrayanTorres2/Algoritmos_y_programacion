737a4b22c0a1298f31a02bb3645133398192f230
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-05-03 19:45:30 -0500

    Create metodoslistas.py

---
+++ Semana5/metodoslistas.py
@@ -0,0 +1,71 @@
+#Métodos de las listas
+#append()
+#clear()
+#extend() une una lista con otra
+#count()
+#index()
+#insert(posicion,elemento)
+#pop()
+#remove()
+#reverse()
+#sort()
+#lista
+tupla=(1,2,3,5,4)#tupla
+pares=[]
+"""
+for i in listas:
+  if(i%2==0):
+    pares.append(i)
+print(pares)
+numero=int(input("DIGITE NUMERO: "))
+pares.append(numero)
+pares.append(3)
+pares.append(2)
+pares.append(1)
+numero=int(input("DIGITE NUMERO: "))
+tupla.append()
+print(tupla)
+#limpiar una listas
+listas.clear()
+listas=[]
+#union de dos listas
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+lista2.extend(lista1)
+print(lista2)
+#contar
+lista1 = [1,2,3,1,2,123,4,2,5,42,13,43,11,1,1]
+lista2 = [4,5,6]
+print(lista1.count(1))
+#posicion
+lista1 = [1,2,3,1,2,123,4,2,5,42,13,43,11,1,1]
+posicion=lista1.index(5)
+print(posicion)
+print(len(lista1))#tamaño de lista
+#insertar en una posicion
+lista1.insert(8,1000)
+print(len(lista1))#tamaño de lista
+print(lista1[9])
+#pop elimina en la ultima posicion
+lista1 = [1,2,3,1,2,123,4,2,5,42,13,43,11,1,1]
+lista1.pop()
+print(len(lista1))
+lista1.pop()
+print(len(lista1))
+lista1.pop()
+print(len(lista1))
+print(lista1)
+#remove
+lista1 = [1,2,3,1,2,123,4,2,5,42,13,43,11,1,1]
+lista1.remove(123)
+print(len(lista1))
+print(lista1)
+#reverse
+lista1 = [1,2,3,1,2,123,4,2,5,42,13,43,11,1,1]
+lista1.sort(reverse=True)
+print(lista1)
+"""
+#reverse
+lista1 = [1,2,3,1,2,123,4,2,5,42,13,43,11,1,1]
+lista1.sort(reverse=True)
+print(lista1)

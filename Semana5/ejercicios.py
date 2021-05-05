528f3e5771911e67bba6e73050721eb76c3d1636
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-05-05 18:23:42 -0500

    Create ejercicios.py

---
+++ Semana5/ejercicios.py
@@ -0,0 +1,10 @@
+contador=0
+for i in range(97,1004):
+  if(i%2==0):
+    contador=contador+i
+print(contador) 
+lista=[]
+for i in range(1,101):
+  if(i%7!=0):
+    lista.append(i)
+print(lista)    
